e5abbbe99fe4e36c894314331cbbca9b400622be
Régis Sébastien Fontaine <>
2021-06-03 14:48:38 +0000

    Add console.log for quick test in terminal

--- index.js
+++ index.js
@@ -2,8 +2,10 @@ const information = require("./information");
 const cowsay = require("cowsay");
 let message = `Hello I'm ${information.user.name} from ${information.user.campus} Campus !`;
 
-cowsay.say({
+let whatTheCowSay = cowsay.say({
   T: 'U ',
-  e:'oO',
-  text: message, 
+  e: 'oO',
+  text: message,
 });
+
+console.log(whatTheCowSay);
