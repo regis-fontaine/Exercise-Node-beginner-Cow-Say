0b654f0300e2cd815f5c68b5c8f606ae1f075e83
Régis Sébastien Fontaine <>
2021-06-03 14:11:50 +0000

    import info, update text, tongue and eyes

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
 const information = require("./information");
+const cowsay = require("cowsay");
+let message = `Hello I'm ${information.user.name} from ${information.user.campus} Campus !`;
 
-console.log(information);
+cowsay.say({
+  T: 'U ',
+  e:'oO',
+  text: message, 
+});
